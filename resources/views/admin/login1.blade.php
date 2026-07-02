c84f5708352cae3c5f2390346635b7eb17c8ed6b
adhya2003 <pkssreeadhya@gmail.com>
2026-07-02 12:09:58 +0530

    Production ready version

---
+++ resources/views/admin/login1.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Sree Adhya Traders</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+  <style type="text/css">
+    body{      background: #F8F9FA;    }
+     .company-name { font-size: 24px; font-weight: bold; color: #1e293b; }
+  </style>
+</head>
+<body>
+    <h2> Login</h2>
+
+    @if ($errors->any())
+        <div style="color: red;">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <form method="POST" action="{{ route('login.post') }}">
+        @csrf {{-- Crucial security mechanism in Laravel --}}
+        <div>
+            <label>Email:</label>
+            <input type="email" name="email" value="{{ old('email') }}" required>
+        </div>
+        <br>
+        <div>
+            <label>Password:</label>
+            <input type="password" name="password" required>
+        </div>
+        <br>
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
