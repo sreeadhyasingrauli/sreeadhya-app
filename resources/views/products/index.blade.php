734278ffb661892813db723be3db517b0fc0ac3c
adhya2003 <pkssreeadhya@gmail.com>
2026-07-04 20:58:17 +0530

    updated products

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -14,7 +14,7 @@
         <div class="card">
             <div class="card-header">Product List</div>
             <div class="card-body">
-                <a href="{{ route('products.create') }}" class="btn btn-success btn-sm my-2"><i class="bi bi-plus-circle"></i> Add New Product</a>
+                <a href="{{ route('products.create') }}" class="btn btn-success btn-sm my-2"><i class="bi bi-plus-circle"></i> Add New Products</a>
                 <table class="table table-striped table-bordered">
                     <thead>
                       <tr>
